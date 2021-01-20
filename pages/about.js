b3d0df0eae80bfb8b7131e6fa93bd26b0e5c3bc6
unknown <runforhealth5k@gmail.com>
2021-01-19 19:44:02 -0800

    added FB icon and centered stuff

--- pages/about.js
+++ pages/about.js
@@ -5,12 +5,12 @@ export default function About() {
   return (
     <Layout>
       <Head>
-        <title>About - {siteTitle}</title><br />
+        <title> About - {siteTitle}</title><br />
       </Head>
       <div className="shadow p-4 bg-white">
-        <div className="text-left">
+        <div className="text-center">
           <h1 className="mb-2 text-gray-700">About</h1>
-          <p className="text-grey-600 text-sm"><b><u>Run for Health:</u></b><br />
+          <p className="text-grey-600 text-md"><b><u>Run for Health:</u></b><br />
 The opportunities that the Shalom Free Clinic has given to innumerable students interested in careers related to
 healthcare run unparalleled in Chico’s community. For the better part of two decades, the clinic has provided
 care to the underinsured and underserved in the area, while allowing students to gain insight about the humanitarian
@@ -29,10 +29,14 @@ The Shalom Free Clinic is supported by many wonderful organizations and individu
 We wish to help others in a vibrant atmosphere that conveys our attitude of compassion, warmth, acceptance, and respect for all people.
 The Clinic is entirely staffed by volunteers, both medical and non-medical, from Chico and the surrounding communities. Volunteers are our
 foundation and we cannot function without them. If you are interested in making a difference in your community by donating your time,
-knowledge, money, or items to the Shalom Free Clinic, please see our <a href="https://shalomfreeclinic.org/volunteer/" className="underline mr-4 text-blue-500 hover:text-blue-400">volunteer page</a>or consider making a donation. </p>
+knowledge, money, or items to the Shalom Free Clinic, please see our <a href="https://shalomfreeclinic.org/volunteer/" className="underline mr-4 text-blue-500 hover:text-blue-400"><b>volunteer page</b></a>
+or consider making a donation.
+          </p>
         </div>
-        <div className="mt-4">
-          <a href="https://www.facebook.com/shalomfreeclinic/" className="underline mr-4 text-blue-500 hover:text-blue-400">Connect with us on Facebook!</a>
+        <div className="text-center">
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+          <a href="https://www.facebook.com/shalomfreeclinic/" class="fa fa-facebook"></a>
+          <a href="https://www.facebook.com/shalomfreeclinic/" className="underline mr-4 text-blue-500 hover:text-blue-400"><b> Connect with us on Facebook!</b></a><br />
           <img src="images/clinic.png" alt="clinic info" width="350" height="450" />
         </div>
       </div>
