4b8b7da10a6ab8843a925e3395283f81588058f3
unknown <runforhealth5k@gmail.com>
2021-01-19 21:16:03 -0800

    added map & whitespace fix

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import Link from 'next/link';
+import Iframe from 'react-iframe';
 
 export default function About() {
   return (
@@ -59,8 +61,8 @@ export default function About() {
             see our{' '}
             <a
               href="https://shalomfreeclinic.org/volunteer/"
-              className="underline mr-4 text-blue-500 hover:text-blue-400">
-              <b>volunteer page</b>
+              className="underline text-blue-500 hover:text-blue-400 font-bold">
+              volunteer page{' '}
             </a>
             or consider making a donation.
           </p>
@@ -71,19 +73,41 @@ export default function About() {
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
           <a
             href="https://www.facebook.com/shalomfreeclinic/"
-            class="fa fa-facebook"></a>
+            className="fa fa-facebook"></a>
           <a
             href="https://www.facebook.com/shalomfreeclinic/"
-            className="underline mr-4 text-blue-500 hover:text-blue-400">
+            className="underline r-4 text-blue-500 hover:text-blue-400">
             <b> Connect with us on Facebook!</b>
           </a>
           <br />
-          <img
-            src="images/clinic.png"
-            alt="clinic info"
-            width="350"
-            height="450"
-          />
+
+          <div className="flex justify-center">
+            <Iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3067.996579869783!2d-121.83877518462455!3d39.73972767944955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808327ec3aa1a78f%3A0xcc8622eb4999192f!2sShalom%20Free%20Clinic!5e0!3m2!1sen!2sus!4v1611117285743!5m2!1sen!2sus"
+              width="600"
+              height="450"
+              frameborder="0"
+              style="border:0;"
+              allowfullscreen=""
+              aria-hidden="false"
+              tabindex="0"
+            />
+          </div>
+          <Link href="https://maps.google.com/maps?z=16&q=1010%2Bmangrove%2Bavenue%2C%2Bsuite%2Bd%2C%2Bchico%2C%2Bca%2B95928">
+            <a className="text-blue-500 font-bold text-xl">
+              1010 Mangrove Avenue, Suite D, Chico, CA 95928
+            </a>
+          </Link>
+          <p>
+            530-342-2445 / FAX 530-342-2002
+            <br />
+            General healthcare every Sunday from 1:00 - 4:00 PM
+            <br />
+            Mental healthcare every Sunday from 1:00 PM - 4:00 PM
+            <br />
+            Alternative healthcare Wednesday 5:00-7:00 PM
+            <br />
+          </p>
         </div>
       </div>
     </Layout>
