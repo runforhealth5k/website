b01a4b09dfb1162333fba99ac8e5a2544033e1ac
Roberto E Pacheco <roberto@repacheco.dev>
2021-01-21 21:03:44 -0800

    Fix registration link

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
                     <a
-                      href="/registration"
+                      href="https://endurancecui.active.com/event-reg/select-race?e=73465434"
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
                       Registration
                     </a>
